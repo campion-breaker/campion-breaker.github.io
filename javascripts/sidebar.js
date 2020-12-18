9949e53f5503b24cacd35aff28f75813ccd7f632
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2020-12-18 15:52:57 -0500

    Created the Campion site and populated it with information and a portion of the case study.

---
+++ javascripts/sidebar.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elementArray = (selector) =>
+    Array.apply(null, document.querySelectorAll(selector));
+  const sectionHeaders = elementArray('#case-study h2').reverse();
+  const sidebarLinks = elementArray('.sidebar ul li a');
+
+  const setActive = (id) => {
+    const newActive = document.querySelector("[href='#" + id + "']");
+    if (newActive.className === 'active') {
+      return;
+    }
+
+    sidebarLinks.forEach((link) => (link.className = ''));
+    newActive.className = 'active';
+  };
+
+  const scrollHandler = () => {
+    const windowVerticalCenter = Math.floor(window.innerHeight / 2);
+    const activeSection = sectionHeaders.find((header) => {
+      return window.scrollY > header.offsetTop - windowVerticalCenter;
+    });
+
+    if (!activeSection) {
+      return;
+    }
+
+    setActive(activeSection.id);
+  };
+
+  document.addEventListener('scroll', scrollHandler);
+});
