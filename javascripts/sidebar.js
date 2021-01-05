859b504f433cefa05cc304309e0c985d7dfe9773
Gabriel De Almeida <Hello@gabrieldealmeida.com>
2021-01-04 19:25:45 -0500

    Modified margins and sizing for mobile version and made sure footer links opened new secure tabs.

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -1,28 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const elementArray = (selector) =>
-    Array.apply(null, document.querySelectorAll(selector));
+document.addEventListener('DOMContentLoaded', () =>
+{
+  const elementArray = selector => Array.apply(null, document.querySelectorAll(selector));
   const sectionHeaders = elementArray('#case-study h2').reverse();
   const sidebarLinks = elementArray('.sidebar ul li a');
 
-  const setActive = (id) => {
+  const setActive = (id) =>
+  {
     const newActive = document.querySelector("[href='#" + id + "']");
-    if (newActive.className === 'active') {
-      return;
-    }
+    if (newActive.className === 'active') { return }
 
-    sidebarLinks.forEach((link) => (link.className = ''));
+    sidebarLinks.forEach((link) => link.className = '');
     newActive.className = 'active';
-  };
+  }
 
-  const scrollHandler = () => {
+  const scrollHandler = () =>
+  {
     const windowVerticalCenter = Math.floor(window.innerHeight / 2);
-    const activeSection = sectionHeaders.find((header) => {
-      return window.scrollY > header.offsetTop - windowVerticalCenter;
+    const activeSection = sectionHeaders.find((header) =>
+    {
+      return window.scrollY > (header.offsetTop - windowVerticalCenter);
     });
 
-    if (!activeSection) {
-      return;
-    }
+    if (!activeSection) { return }
 
     setActive(activeSection.id);
   };
